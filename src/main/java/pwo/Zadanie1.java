40f998243048c3f71ac61e0060de11fcd9397bda
Maciej Rak <maciej.rak@pollub.edu.pl>
2022-10-19 19:13:45 +0200

    Wykonanie Zadania 2 - wersja 1.
    LucasGenerator działa podobnie do FG

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -12,6 +12,7 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
+    public static final String FILE_NAME2 = "Lucasseq100.txt";
 
     public static void main(String[] args) {
 
@@ -19,12 +20,26 @@ public class Zadanie1 {
 
         boolean ok = SequenceTools.writeToFile(
                 new FibonacciGenerator(), FROM, TO, FILE_NAME);
-
+                
         if (ok) {
             System.out.println("Wynik zapisany do pliku: "
                     + FILE_NAME);
         } else {
             System.out.println("Błąd");
         }
+        
+        
+        System.out.println("Lucas wersja 1");
+        
+        boolean ok2 = SequenceTools.writeToFile2(
+                new LucasGenerator(), FROM, TO, FILE_NAME2);
+                
+        if (ok2) {
+            System.out.println("Wynik zapisany do pliku: "
+                    + FILE_NAME2);
+        } else {
+            System.out.println("Błąd");
+        }
+      
     }
 }
